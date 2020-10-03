8b4265e390e9ac8219120abde8590c1d60b3ffbc
Harrison J Goldwyn <goldwyn@uw.edu>
2020-10-02 17:03:18 -0700

    Fixed bugs in derivation of z-oriented fields Fixed this yesterday and forgot to commit it

--- optics/anal_foc_diff_fields.py
+++ optics/anal_foc_diff_fields.py
@@ -17,16 +17,18 @@ def E_field(dipole_orientation_angle, xi, y, k):
     """ Defines the analytics approximation to the focused and
         diffracted field for dipole oriented in the
         focal plane at an angle 'dipole_orientation_angle' from the
-        x-axis
+        x-axis.
         """
-    # print('inside anal_foc_diff_fields.E_field, \n',
-    #     'xi.shape = ',xi.shape,'\n',
-    #     'y.shape = ',y.shape,
-    #     'k = ',k
-    #     # 'k.shape = ',k.shape
-    #     )
+
     psi = dipole_orientation_angle
     phi_P = phi(xi, y) - psi
+
+    ## Define Bessel ratios with limits explicitly to ovoid divergent division
+    sphj1_on_krho = spf.spherical_jn( 1, k*rho(xi, y) )/(k*rho(xi, y))
+    sphj1_on_krho[k*rho(xi, y) == 0] = 1/3
+    ##
+    j2_on_krho = spf.jv(2, k*rho(xi, y) )/(k*rho(xi, y))
+    j2_on_krho[k*rho(xi, y) == 0] = 0
 
     E_xP = (
             (
@@ -35,7 +37,7 @@ def E_field(dipole_orientation_angle, xi, y, k):
                 np.cos( 2*(phi_P) )
                 )
             *
-            np.nan_to_num(spf.spherical_jn( 1, k*rho(xi, y) )/(k*rho(xi, y)))
+            sphj1_on_krho
         +
         (
             np.sin( phi_P )**2.
@@ -54,9 +56,7 @@ def E_field(dipole_orientation_angle, xi, y, k):
 
     # print("rho(xi, y) = ",rho(xi, y))
     # print("xi, y = ",xi, ' ',y)
-    E_zP = -np.cos(phi_P) * np.nan_to_num(
-        spf.jv(2, k*rho(xi, y) )/(k*rho(xi, y))
-        )
+    E_zP = -np.cos(phi_P) * j2_on_krho
 
 
     E_x = np.cos(psi)*E_xP - np.sin(psi)*E_yP
@@ -65,30 +65,73 @@ def E_field(dipole_orientation_angle, xi, y, k):
 
     return np.array([E_x, E_y, E_z])*k**3.
 
-# def anal_psf(ori, xi, y, k):
-#     inte_psf = (
-#         np.abs(E_x(ori, xi, y, k))**2.
-#         +
-#         np.abs(E_y(ori, xi, y, k))**2.
-#         +
-#         np.abs(E_z(ori, xi, y, k))**2.
-#         )
-#     return inte_psf
 
-# def anal_interf(ori, x, y, k, d):
-#     first_term = (
-#         (E_x(ori, x, y, k))*np.conj(E_x(ori, x-d, y, k))
-#         +
-#         (E_y(ori, x, y, k))*np.conj(E_y(ori, x-d, y, k))
-#         +
-#         (E_z(ori, x, y, k))*np.conj(E_z(ori, x-d, y, k))
-#         )
-#     second = (
-#         (E_x(ori, x-d, y, k))*np.conj(E_x(ori, x, y, k))
-#         +
-#         (E_y(ori, x-d, y, k))*np.conj(E_y(ori, x, y, k))
-#         +
-#         (E_z(ori, x-d, y, k))*np.conj(E_z(ori, x, y, k))
-#         )
-#     return first_term+second
+def E_pz(xi, y, k):
+    """ Defines the analytics approximation to the focused and
+        diffracted field for dipole oriented along the optical axis.
+        """
+    phi_P = phi(xi, y)
+
+    j2_on_krho = spf.jv(2, k*rho(xi, y) )/(k*rho(xi, y))
+    j2_on_krho[k*rho(xi, y) == 0] = 0
+
+    sphj0_on_krhosqrd = spf.spherical_jn(0, k*rho(xi, y) )/(k*rho(xi, y))**2.
+    sphy1_plus_sphj0_on_krhosqrd = (
+        spf.spherical_yn(1, k*rho(xi, y) )
+        +
+        sphj0_on_krhosqrd
+        )
+    sphy1_plus_sphj0_on_krhosqrd[k*rho(xi, y) == 0] = -2/3
+
+
+    E_x = 1j*(
+        j2_on_krho
+        *
+        np.cos(phi_P)
+        )
+
+    E_y = -1j*(
+        j2_on_krho
+        *
+        np.sin(phi_P)
+        )
+
+    E_z = -sphy1_plus_sphj0_on_krhosqrd
+
+    return np.array([E_x, E_y, E_z])*k**3.
+
+
+def old_E_pz(xi, y, k):
+    """ Defines the analytics approximation to the focused and
+        diffracted field for dipole oriented along the optical axis.
+        """
+    phi_P = phi(xi, y)
+
+    j2_on_krho = spf.jv(2, k*rho(xi, y) )/(k*rho(xi, y))
+    j2_on_krho[k*rho(xi, y) == 0] = 0
+
+    sphj0_on_krhosqrd = spf.spherical_jn(0, k*rho(xi, y) )/(k*rho(xi, y))**2.
+    sphy1_plus_sphj0_on_krhosqrd = (
+        spf.spherical_yn(1, k*rho(xi, y) )
+        +
+        sphj0_on_krhosqrd
+        )
+    sphy1_plus_sphj0_on_krhosqrd[k*rho(xi, y) == 0] = -2/3
+
+
+    E_x = (
+        j2_on_krho
+        *
+        np.cos(phi_P)
+        )
+
+    E_y = -(
+        j2_on_krho
+        *
+        np.sin(phi_P)
+        )
+
+    E_z = 2/3 * spf.spherical_jn(0, k*rho(xi, y)) - 1/3*spf.spherical_jn(2, k*rho(xi, y))
+
+    return np.array([E_x, E_y, E_z])*k**3.
 
